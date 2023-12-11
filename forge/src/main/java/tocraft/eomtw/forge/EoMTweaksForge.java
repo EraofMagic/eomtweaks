3859b2f1a397dc5eb27ace86791fcc460085546a
ToCraft <piminecraft492@gmail.com>
2023-12-11 19:49:18 +0100

    add tin

--- forge/src/main/java/tocraft/eomtw/forge/EoMTweaksForge.java
+++ forge/src/main/java/tocraft/eomtw/forge/EoMTweaksForge.java
@@ -5,8 +5,9 @@ import tocraft.eomtw.EoMTweaks;
 
 @Mod(EoMTweaks.MODID)
 public class EoMTweaksForge {
-
+			
 	public EoMTweaksForge() {
 		new EoMTweaks().initialize();
+		EoMRegistry.register();
 	}
 }
