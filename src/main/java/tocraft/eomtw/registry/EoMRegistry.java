b1218361ff5d02a9bf8b59305a9c07a2bd20f846
robdu <Lucien.zeiske@gmail.com>
2024-03-06 18:20:43 +0100

    Added Unfired, fire & seared brick Added Firebrick Removed Uneccesary Cokeoven brick png

--- src/main/java/tocraft/eomtw/registry/EoMRegistry.java
+++ src/main/java/tocraft/eomtw/registry/EoMRegistry.java
@@ -56,6 +56,14 @@ public class EoMRegistry {
         public static final RegistryObject<Item> TIN_DOOR = ITEMS.register("tin_door", () -> new DoubleHighBlockItem(Blocks.TIN_DOOR.get(), (new Item.Properties()).tab(CreativeModeTab.TAB_REDSTONE)));
         public static final RegistryObject<Item> TIN_TRAPDOOR = ITEMS.register("tin_trapdoor", () -> new BlockItem(Blocks.TIN_TRAPDOOR.get(), (new Item.Properties()).tab(CreativeModeTab.TAB_REDSTONE)));
 
+        public static final RegistryObject<Item> FIRE_BRICK = ITEMS.register("fire_brick", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+        public static final RegistryObject<Item> UNFIRED_FIRE_BRICK = ITEMS.register("unfired_fire_brick", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+        public static final RegistryObject<Item> UNFIRED_COKE_BRICK = ITEMS.register("unfired_coke_brick", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+        public static final RegistryObject<Item> UNFIRED_SEARED_BRICK = ITEMS.register("unfired_seared_brick", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
         public static void register(IEventBus eventBus) {
             ITEMS.register(eventBus);
         }
