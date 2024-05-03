7ca0e9aad6c187d313d7eaae94dfea94d62f6827
robdu <Lucien.zeiske@gmail.com>
2024-05-03 21:33:16 +0200

    Added EomDump command to dump item registryname strings for sawdust tag

---
+++ src/main/java/tocraft/eomtw/registry/CommandRegistry.java
@@ -0,0 +1,18 @@
+package tocraft.eomtw.registry;
+
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+import tocraft.eomtw.commands.eomDump;
+
+@Mod.EventBusSubscriber(modid = "eomtw")
+public class CommandRegistry {
+
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+        new eomDump(event.getDispatcher());
+
+        ConfigCommand.register(event.getDispatcher());
+    }
+}
